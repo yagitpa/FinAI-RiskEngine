fd53ce5058952f14bd54430d55c26b9d26c1ddee
yagitpa <yakov.palchikov@gmail.com>
2026-04-18 07:15:57 +0700

    Initial commit

---
+++ src/main/java/com/example/finai_riskengine/FinAiRiskEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.finai_riskengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinAiRiskEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinAiRiskEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finai_riskengine/FinAiRiskEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finai_riskengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinAiRiskEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
